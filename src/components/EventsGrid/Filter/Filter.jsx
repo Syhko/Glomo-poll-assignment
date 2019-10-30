17fd491f6c99438c69333afafe7dc7ebf1d49a9e
Syhko <alexandregrauby@gmail.com>
2019-10-30 14:58:34 +0100

    adding filtering

--- src/components/EventsGrid/Filter/Filter.jsx
+++ src/components/EventsGrid/Filter/Filter.jsx
@@ -1,26 +1,61 @@
 import React from "react";
 import "./style.scss";
 
-const Filter = ({ allSports, defaultSport, handleSport }) => {
-  const [isOpen, setIsOpen] = React.useState(false);
+const Filter = ({
+  allSports,
+  allEvents,
+  defaultSport,
+  handleSport,
+  handleCountry
+}) => {
+  const [isSportOpen, setIsSportOpen] = React.useState(false);
   const [sportSelected, setSportSelected] = React.useState(defaultSport);
+  const [isCountryOpen, setIsCountryOpen] = React.useState(false);
+  const [countrySelected, setCountrySelected] = React.useState("Choose...");
   const selectSport = sport => {
     setSportSelected(sport);
-    setIsOpen(false);
+    setIsSportOpen(false);
     handleSport(sport);
   };
+  const selectCountry = country => {
+    setCountrySelected(country);
+    setIsCountryOpen(false);
+    handleCountry(sportSelected, country);
+  };
+  const countries = [
+    ...new Set(
+      allEvents
+        .filter(event => event.sport === sportSelected)
+        .map(event => event.country)
+    )
+  ];
+  console.log(countries);
   return (
     <div className="filter-wrapper">
       <div className="sport-filter">
-        <h1 onClick={() => setIsOpen(!isOpen)}>
-          {sportSelected}
-          {!isOpen && <span> &#9660;</span>}
-          {isOpen && <span> &#9650;</span>}
+        <h1 onClick={() => setIsSportOpen(!isSportOpen)}>
+          Sport {sportSelected}
+          {!isSportOpen && <span> &#9660;</span>}
+          {isSportOpen && <span> &#9650;</span>}
         </h1>
-        {isOpen && (
+        {isSportOpen && (
           <div className="dropdown">
             {allSports.map(sport => (
               <p onClick={() => selectSport(sport)}>{sport}</p>
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="sport-filter">
+        <h1 onClick={() => setIsCountryOpen(!isCountryOpen)}>
+          Country {countrySelected}
+          {!isCountryOpen && <span> &#9660;</span>}
+          {isCountryOpen && <span> &#9650;</span>}
+        </h1>
+        {isCountryOpen && (
+          <div className="dropdown">
+            {countries.map(country => (
+              <p onClick={() => selectCountry(country)}>{country}</p>
             ))}
           </div>
         )}
